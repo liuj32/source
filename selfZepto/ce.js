21bc8b80101eb4d6f7e7e8cef8b4f5b26a64c99f
liujie <593177117@qq.com>
2019-11-24 15:02:11 +0800

    11/24

--- selfZepto/ce.js
+++
@@ -1,5 +0,0 @@
-var data = 'windows 88 is ok';
-// data.match(/windows (?=\d+)/);  // ["windows "]
-// data.match(/windows (?:\d+)/);  // ["windows 98"]
-// data.match(/windows (\d+)/);    // ["windows 98", "98"]
-console.log(data.match(/windows (\d)(\d)/))
